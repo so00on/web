1be858df953232f6f535f88e6843c06041724a6a
so00n <soonjack135@gmail.com>
2023-11-21 00:13:19 -0400

    documentacion de una carpeta urls

--- Calculadora/calculadora/agua/urls.py
+++ Calculadora/calculadora/agua/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path('formulario', views.formulario, name='formulario'),
     path('result', views.result, name='result')
 ]
+
+# indican cómo deben interpretarse y manejar las solicitudes HTTP entrantes en tu aplicación Django. Cuando un usuario accede a una URL específica, Django utiliza estas definiciones de ruta para dirigir la solicitud a la función de vista correspondiente, que procesará la solicitud y devolverá una respuesta. El uso de nombres (name) permite referenciar estas rutas de manera más fácil en otras partes del código, como en las plantillas HTML o en el propio código Python.
+
+
+
+
+
+
