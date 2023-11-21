7821ae6e72d0468f849aa96b9c742f87ad9f2add
so00n <soonjack135@gmail.com>
2023-11-21 00:11:35 -0400

    documentacion de otras  carpetas

--- Calculadora/calculadora/agua/admin.py
+++ Calculadora/calculadora/agua/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
+#Esto te permite definir modelos en tu aplicación y luego administrar esos modelos a través de la interfaz de administración de Django, que incluye la capacidad de agregar, editar y eliminar instancias de esos modelos.
--- Calculadora/calculadora/agua/apps.py
+++ Calculadora/calculadora/agua/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class AguaConfig(AppConfig):
     name = 'agua'
+
+
+#Define una nueva clase llamada AguaConfig que hereda de AppConfig. En esta clase, se establece el atributo name en 'agua'. El atributo name especifica el nombre de la aplicación y debe coincidir con el nombre de la carpeta que contiene la aplicación.En este caso, parece que esta aplicación se llama 'agua'.Cuando defines una clase AppConfig, puedes realizar varias configuraciones adicionales, como configurar señales, configurar rutas de archivos estáticos, o realizar otras tareas de inicialización específicas de la aplicación.
+
+
+
+
+
+
